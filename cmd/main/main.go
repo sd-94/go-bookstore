86f82b96df45edb33f5079e758dd72d2f0ffb544
Artem Dragosh <sd9265479529@gmail.com>
2024-06-24 19:50:58 +0300

    Add main.go

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"go-booksore/pkg/routes"
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:3306", r))
+}
