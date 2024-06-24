f141b53a990700f3a622f66c8cd1b28b6c8a1481
Artem Dragosh <sd9265479529@gmail.com>
2024-06-24 19:43:09 +0300

    Add models

---
+++ pkg/models/book.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Name string `json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init(){
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
